8225c65ed4f3bab81a0ef3356bfb51fd4d84451f
Tanknguyen1610 <tanknguyen1610@gmail.com>
2025-10-29 16:36:54 +0700

    [ADD] Register form

--- Recruitment_Frontend/src/main.jsx
+++ Recruitment_Frontend/src/main.jsx
@@ -27,10 +27,10 @@ const router = createBrowserRouter([
         path: "login",
         element: <LoginPage />
       },
-      // {
-      //   path: "register",
-      //   element: <RegisterPage />
-      // },
+      {
+        path: "register",
+        element: <RegisterPage />
+      },
       // {
       //   path: "test-google",
       //   element: <GoogleLoginTest />
